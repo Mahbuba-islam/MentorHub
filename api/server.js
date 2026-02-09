45b1870e99e71958b7a7e73bd1752b17ae91f3b7
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-02-09 13:59:15 -0500

    Fix PORT type for Render

--- api/server.js
+++ api/server.js
@@ -9,8 +9,9 @@ async function main() {
   try {
     await prisma.$connect();
     console.log("connected to database");
-    index_default.listen(PORT, () => {
-      console.log(`server is running on port ${PORT}`);
+    const PORT2 = Number(process.env.PORT) || 5e3;
+    index_default.listen(PORT2, "0.0.0.0", () => {
+      console.log(`server is running on port ${PORT2}`);
     });
   } catch (err) {
     console.log(err);
--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,13 @@ async function main() {
         await prisma.$connect();
         console.log("connected to database");
 
-        app.listen(PORT, () => {
-            console.log(`server is running on port ${PORT}`);
-        });
+    const PORT = Number(process.env.PORT) || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+
     } catch (err) {
         console.log(err);
         await prisma.$disconnect();
