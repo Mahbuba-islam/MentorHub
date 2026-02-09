378d803200058a9a98fc07fa8bbea8f7a701a1a7
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-02-09 13:07:41 -0500

    Final working build for Render deployment

---
+++ api/index.js
@@ -0,0 +1,6 @@
+import {
+  index_default
+} from "./chunk-7BKPH2BP.js";
+export {
+  index_default as default
+};
---
+++ api/server.js
@@ -0,0 +1,21 @@
+import {
+  index_default,
+  prisma
+} from "./chunk-7BKPH2BP.js";
+
+// src/server.ts
+var PORT = process.env.PORT || 5e3;
+async function main() {
+  try {
+    await prisma.$connect();
+    console.log("connected to database");
+    index_default.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+}
+main();
