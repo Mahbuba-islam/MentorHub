7e2e71372a4c485670918e7e33c056ad1e853c87
Mahbuba Islam <mahbubaislam7010@gmail.com>
2026-02-09 18:33:51 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,8 @@ const app = express()
 app.use(
   cors({
     origin: [
-      
-      "https://mentor-hub-client.vercel.app/"
+       "http://localhost:3000",
+       "https://mentor-hub-client.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
